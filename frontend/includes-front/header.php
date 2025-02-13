69b60ffff4e72ca12c5910b87e780bbbbf68deca
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2025-02-13 10:49:37 -0500

    Creando la estructura básica de archivos

---
+++ frontend/includes-front/header.php
@@ -0,0 +1,10 @@
+<header>
+    <span>Mi tienda</span>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="productos.php">Productos</a></li>
+            <li><a href="carrito.php">Carrito</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow">
+    <!--Facebook y otras redes.-->
+    <meta property="og:title" content="Tu Tienda Online - Encuentra lo Mejor">
+    <meta property="og:description" content="Explora nuestra tienda y disfruta de grandes ofertas y envíos rápidos. ¡Compra ahora!">
+    <meta property="og:image" content="URL_de_la_imagen_destacada">
+    <meta property="og:url" content="https://www.tutienda.com">
+    <meta property="og:type" content="website">
+    <meta name="twitter:card" content="summary_large_image">
+    <!--Para Compartir en Twitter/X-->
+    <meta name="twitter:title" content="Tu Tienda Online - Encuentra lo Mejor">
+    <meta name="twitter:description" content="Explora nuestra tienda y disfruta de grandes ofertas y envíos rápidos. ¡Compra ahora!">
+    <meta name="twitter:image" content="URL_de_la_imagen_destacada">
+    <link rel="canonical" href="https://www.tutienda.com/pagina-principal">
+    <meta name="description" content="Descubre los mejores productos al mejor precio. Envíos rápidos, ofertas exclusivas y una experiencia de compra fácil y segura. ¡Compra ahora!">
+    <title>Tu Tienda Online | Productos de Calidad al Mejor Precio</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./frontend/mobil-styles.css" media="screen and (min-width: 360px)">
+    <link rel="stylesheet" href="./frontend/tablet-styles.css" media="screen and (min-width: 768px)">
+    <link rel="stylesheet" href="./frontend/desktop-styles.css" media="screen and (min-width: 1300px)">
+</head>
+<body>
+    <?php include './frontend/includes-front/header.php'?>
+    <main>
+
+    </main>
+    <?php include './frontend/includes-front/footer.php'?>
+</body>
+</html>
